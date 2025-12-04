bb82f99c93bf6b9030ff7f985a94141b061a2cdb
Muhammad Hamza Khan <89472721+hamzakhan2001@users.noreply.github.com>
2025-12-04 08:19:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -54,4 +54,6 @@ app.use("/api", publicBlogRoutes);
 
 // ✅ 7️⃣ Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running", PORT));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
